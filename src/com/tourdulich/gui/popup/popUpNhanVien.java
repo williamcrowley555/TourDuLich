d6be533fb4d73914607acf3a7de4fda90db0de0b
HaMinhKhoi29092000 <haminhkhoi.29092000.14@gmail.com>
2021-02-20 11:09:25 +0700

    Fix : Adding items to comboBoxVaiTro in NhanVienPopup

--- src/com/tourdulich/gui/popup/popUpNhanVien.java
+++ src/com/tourdulich/gui/popup/popUpNhanVien.java
@@ -8,6 +8,7 @@ package com.tourdulich.gui.popup;
 import com.tourdulich.bll.IVaiTroBLL;
 import com.tourdulich.bll.impl.VaiTroBLL;
 import com.tourdulich.dto.NhanVienDTO;
+import com.tourdulich.dto.VaiTroDTO;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
@@ -25,10 +26,13 @@ import com.tourdulich.gui.menu.MyComboBoxRenderer;
 import com.tourdulich.util.ImageUtil;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.DefaultComboBoxModel;
 
 /**
  *
@@ -41,20 +45,29 @@ public class popUpNhanVien extends javax.swing.JFrame {
     
     public popUpNhanVien(String action) {
         initComponents();
+        
         this.action = action;    
         vaiTroBLL = new VaiTroBLL();
+        
         CustomWindow();
+        myTextArea();
+        setComboBox(comboBoxVaiTro, getVaiTroItems());
         comboBoxVaiTro = myComboBox(comboBoxVaiTro, new Color(14,142,233));
+        
         this.setVisible(true);    
     }
     
     public popUpNhanVien(String action, NhanVienDTO nhanVien) {
         initComponents();
         this.action = action;   
-        vaiTroBLL = new VaiTroBLL();   
+        vaiTroBLL = new VaiTroBLL(); 
+        
         CustomWindow();
+        myTextArea();
+        setComboBox(comboBoxVaiTro, getVaiTroItems());
         comboBoxVaiTro = myComboBox(comboBoxVaiTro, new Color(14,142,233));
         setLabelText(nhanVien);
+        
         this.setVisible(true);    
     }
     
@@ -74,8 +87,28 @@ public class popUpNhanVien extends javax.swing.JFrame {
             }
         txtDiaChi.setText(nhanVien.getDiaChi());
         txtSDT.setText(nhanVien.getSdt());
-//        comboBoxVaiTro.setSelectedItem(data.get(7));
+        comboBoxVaiTro.setSelectedItem(getVaiTroItemName(vaiTroBLL.findById(nhanVien.getIdVaiTro())));
+        
         //lblAnh.setText(data.get(7).toString());
+    }
+    
+    public void setComboBox(JComboBox<String> comboBox, String[] listItems) {
+        comboBox.setModel(new DefaultComboBoxModel<>(listItems));
+    } 
+    
+    public String[] getVaiTroItems() {
+        List<VaiTroDTO> vaiTroLists = vaiTroBLL.findAll();
+        String[] vaiTroItems = new String[vaiTroLists.size()];
+        int index = 0;
+        for(VaiTroDTO vt : vaiTroLists) {
+            vaiTroItems[index] = vt.getId() + " - " + vt.getTenVaiTro();
+            ++ index;
+        }
+        return vaiTroItems;
+    }
+    
+    public String getVaiTroItemName(VaiTroDTO vaiTro) {
+        return vaiTro.getId() + " - " + vaiTro.getTenVaiTro();
     }
     
     public popUpNhanVien() {
@@ -86,7 +119,7 @@ public class popUpNhanVien extends javax.swing.JFrame {
     public void CustomWindow()
     {   
         Color flatBlue = new Color(14,142,233);  
-        this.setSize(new Dimension(548,344));
+        this.setSize(new Dimension(615,344));
         this.getRootPane().setBorder(BorderFactory.createMatteBorder(0,1,1,1, flatBlue));   
         center();
         lblMinimize.setText("\u2014");
@@ -95,6 +128,11 @@ public class popUpNhanVien extends javax.swing.JFrame {
      
     }
     
+    public void myTextArea()
+    {
+        txtDiaChi.setWrapStyleWord(true);
+        txtDiaChi.setLineWrap(true);
+    }
     public JComboBox myComboBox(JComboBox box, Color color)
     {   
         box.setRenderer(new MyComboBoxRenderer());
@@ -155,7 +193,6 @@ public class popUpNhanVien extends javax.swing.JFrame {
         radioNam = new javax.swing.JRadioButton();
         radioNu = new javax.swing.JRadioButton();
         lblTen = new javax.swing.JLabel();
-        txtDiaChi = new javax.swing.JTextField();
         lblVaiTro = new javax.swing.JLabel();
         comboBoxVaiTro = new javax.swing.JComboBox<>();
         txtSDT = new javax.swing.JTextField();
@@ -164,6 +201,8 @@ public class popUpNhanVien extends javax.swing.JFrame {
         btnHuy = new javax.swing.JButton();
         lblNgaySinh = new javax.swing.JLabel();
         DCNgaySinh = new com.toedter.calendar.JDateChooser();
+        AreaScrollPane = new javax.swing.JScrollPane();
+        txtDiaChi = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -201,7 +240,7 @@ public class popUpNhanVien extends javax.swing.JFrame {
         panelHeaderLayout.setHorizontalGroup(
             panelHeaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelHeaderLayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(517, Short.MAX_VALUE)
                 .addComponent(lblMinimize, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
                 .addComponent(lblExit, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -268,9 +307,6 @@ public class popUpNhanVien extends javax.swing.JFrame {
         lblTen.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         lblTen.setText("Tên:");
 
-        txtDiaChi.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        txtDiaChi.setBorder(javax.swing.BorderFactory.createMatteBorder(2, 2, 2, 2, new java.awt.Color(204, 204, 204)));
-
         lblVaiTro.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         lblVaiTro.setText("Vai trò:");
 
@@ -323,6 +359,15 @@ public class popUpNhanVien extends javax.swing.JFrame {
 
         DCNgaySinh.setDateFormatString("yyyy-MM-dd\n\n");
 
+        AreaScrollPane.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        AreaScrollPane.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        txtDiaChi.setColumns(20);
+        txtDiaChi.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        txtDiaChi.setRows(5);
+        txtDiaChi.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 2, 2, new java.awt.Color(204, 204, 204)));
+        AreaScrollPane.setViewportView(txtDiaChi);
+
         javax.swing.GroupLayout pnlBodyLayout = new javax.swing.GroupLayout(pnlBody);
         pnlBody.setLayout(pnlBodyLayout);
         pnlBodyLayout.setHorizontalGroup(
@@ -333,17 +378,6 @@ public class popUpNhanVien extends javax.swing.JFrame {
                     .addComponent(lblAnh, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(btnChonAnh, javax.swing.GroupLayout.DEFAULT_SIZE, 149, Short.MAX_VALUE))
                 .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(pnlBodyLayout.createSequentialGroup()
-                        .addGap(26, 26, 26)
-                        .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(lblDiaChi)
-                            .addComponent(lblNgaySinh)
-                            .addComponent(txtDiaChi)
-                            .addComponent(DCNgaySinh, javax.swing.GroupLayout.DEFAULT_SIZE, 170, Short.MAX_VALUE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 63, Short.MAX_VALUE)
-                        .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(btnLuu, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(btnHuy, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(pnlBodyLayout.createSequentialGroup()
                         .addGap(25, 25, 25)
                         .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -367,20 +401,24 @@ public class popUpNhanVien extends javax.swing.JFrame {
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlBodyLayout.createSequentialGroup()
                                 .addComponent(lblGioiTinh)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(radioNam)))))
+                                .addComponent(radioNam))))
+                    .addGroup(pnlBodyLayout.createSequentialGroup()
+                        .addGap(26, 26, 26)
+                        .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblDiaChi)
+                            .addComponent(lblNgaySinh)
+                            .addComponent(DCNgaySinh, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(AreaScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 231, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnLuu, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnHuy, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addGap(20, 20, 20))
         );
         pnlBodyLayout.setVerticalGroup(
             pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(pnlBodyLayout.createSequentialGroup()
-                .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(pnlBodyLayout.createSequentialGroup()
-                        .addGap(20, 20, 20)
-                        .addComponent(lblAnh, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(btnChonAnh, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txtDiaChi, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(pnlBodyLayout.createSequentialGroup()
                         .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(pnlBodyLayout.createSequentialGroup()
@@ -403,41 +441,48 @@ public class popUpNhanVien extends javax.swing.JFrame {
                                 .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                     .addComponent(lblGioiTinh)
                                     .addComponent(radioNam))))
-                        .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addGap(13, 13, 13)
+                        .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblSDT)
+                            .addComponent(txtSDT, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(radioNu))
+                        .addGap(18, 18, 18)
+                        .addComponent(lblNgaySinh)
+                        .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(pnlBodyLayout.createSequentialGroup()
-                                .addGap(61, 61, 61)
-                                .addComponent(lblNgaySinh)
-                                .addGap(18, 18, 18)
+                                .addGap(12, 12, 12)
                                 .addComponent(DCNgaySinh, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(lblDiaChi, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(7, 7, 7))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlBodyLayout.createSequentialGroup()
-                                .addGap(13, 13, 13)
-                                .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(lblSDT)
-                                    .addComponent(txtSDT, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(radioNu))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(pnlBodyLayout.createSequentialGroup()
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(btnLuu, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)))
-                        .addComponent(btnHuy, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(33, Short.MAX_VALUE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
+                        .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(btnHuy, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(AreaScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(pnlBodyLayout.createSequentialGroup()
+                        .addGap(20, 20, 20)
+                        .addGroup(pnlBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(lblDiaChi, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblAnh, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(btnChonAnh, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(57, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelHeader, javax.swing.GroupLayout.DEFAULT_SIZE, 548, Short.MAX_VALUE)
-            .addComponent(pnlBody, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(panelHeader, javax.swing.GroupLayout.DEFAULT_SIZE, 615, Short.MAX_VALUE)
+            .addComponent(pnlBody, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(panelHeader, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(pnlBody, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 0, 0)
+                .addComponent(pnlBody, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -520,6 +565,7 @@ public class popUpNhanVien extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane AreaScrollPane;
     private com.toedter.calendar.JDateChooser DCNgaySinh;
     private javax.swing.JButton btnChonAnh;
     private javax.swing.ButtonGroup btnGroupGioiTinh;
@@ -540,7 +586,7 @@ public class popUpNhanVien extends javax.swing.JFrame {
     private javax.swing.JPanel pnlBody;
     private javax.swing.JRadioButton radioNam;
     private javax.swing.JRadioButton radioNu;
-    private javax.swing.JTextField txtDiaChi;
+    private javax.swing.JTextArea txtDiaChi;
     private javax.swing.JTextField txtHo;
     private javax.swing.JTextField txtSDT;
     private javax.swing.JTextField txtTen;
