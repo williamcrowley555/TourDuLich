9e461066d6dec4b53dac0d873d2aecf08a6a65a7
HaMinhKhoi29092000 <haminhkhoi.29092000.14@gmail.com>
2021-04-30 20:49:27 +0700

    add more testcases in tour, dichvu, loaidulich DAL and BLL testclass

--- test/com/tourdulich/bll/ILoaiDuLichBLLTest.java
+++ test/com/tourdulich/bll/ILoaiDuLichBLLTest.java
@@ -14,6 +14,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 /**
  *
@@ -24,7 +26,8 @@ public class ILoaiDuLichBLLTest {
     public ILoaiDuLichBLLTest() {
     }
     
-  
+   @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void testFindAll() {
@@ -35,18 +38,68 @@ public class ILoaiDuLichBLLTest {
         assertNotNull(result);
     }
 
+    
     @Test
-    public void testFindById() {
-        System.out.println("findById");
+    public void testFindByIdValid() {
+        System.out.println("findByIdValid");
         Long id = 1L;
+        ILoaiDuLichBLL instance = new LoaiDuLichBLL();
+        LoaiDuLichDTO result = instance.findById(id);
+        assertEquals(id, result.getId());
+        
+    }
+    
+    @Test
+    public void testFindByIdInputNull() {
+        System.out.println("findByIdInputNull");
+        Long id = null;
+        ILoaiDuLichBLL instance = new LoaiDuLichBLL();
+        LoaiDuLichDTO result = instance.findById(id);
+        assertNull(result);
+    }
+    
+    @Test
+    public void testFindByIdNotExist() {
+        System.out.println("findByIdNotExist");
+        Long id = 100L;
+        ILoaiDuLichBLL instance = new LoaiDuLichBLL();
+        LoaiDuLichDTO result = instance.findById(id);
+        assertNull(result);
+    }
+    
+    @Test
+    public void testFindByIdInputString() {
+        System.out.println("findByIdInputString");
+        thrown.expect(NumberFormatException.class);
+        Long id = Long.parseLong("abc");
+        ILoaiDuLichBLL instance = new LoaiDuLichBLL();
+        LoaiDuLichDTO result = instance.findById(id);
+        assertEquals(id, result.getId());
+    }
+    
+    @Test
+    public void testFindByIdInputFloat() {
+        System.out.println("findByIdInputFloat");
+        thrown.expect(NumberFormatException.class);
+        Long id = Long.parseLong("1.5");
+        ILoaiDuLichBLL instance = new LoaiDuLichBLL();
+        LoaiDuLichDTO result = instance.findById(id);
+        assertEquals(id, result.getId());
+    }
+    
+    @Test
+    public void testFindByIdInputSpecialCharacters() {
+        System.out.println("findByIdInputSpecialCharacters");
+        thrown.expect(NumberFormatException.class);
+        Long id = Long.parseLong("@b1");
         ILoaiDuLichBLL instance = new LoaiDuLichBLL();
         LoaiDuLichDTO result = instance.findById(id);
         assertEquals(id, result.getId());
     }
 
     @Test
-    public void testSave() {
-        System.out.println("save");
+    public void testSaveValid() {
+        System.out.println("saveValid");
         String tenLoaiDuLich = "Du lịch trong nước";
         LoaiDuLichDTO loaiDuLich = new LoaiDuLichDTO();
         loaiDuLich.setTenLoaiDuLich(tenLoaiDuLich);
@@ -55,11 +108,37 @@ public class ILoaiDuLichBLLTest {
         LoaiDuLichDTO result = instance.findById(saveId);   
         assertEquals(saveId, result.getId());
     }
+    
+    @Test
+    public void testSaveInValid() {
+        System.out.println("saveInValid");
+        String tenLoaiDuLich = "!@#!123";
+        LoaiDuLichDTO loaiDuLich = new LoaiDuLichDTO();
+        loaiDuLich.setTenLoaiDuLich(tenLoaiDuLich);
+        ILoaiDuLichBLL instance = new LoaiDuLichBLL();
+        Long saveId = instance.save(loaiDuLich);
+        LoaiDuLichDTO result = instance.findById(saveId);   
+        assertNull(result);
+    }
+    
+    @Test
+    public void testSaveNull() {
+        System.out.println("saveNull");
+        String tenLoaiDuLich = null;
+        LoaiDuLichDTO loaiDuLich = new LoaiDuLichDTO();
+        loaiDuLich.setTenLoaiDuLich(tenLoaiDuLich);
+        ILoaiDuLichBLL instance = new LoaiDuLichBLL();
+        Long saveId = instance.save(loaiDuLich);
+        LoaiDuLichDTO result = instance.findById(saveId);   
+        assertNull(result);
+    }
+    
+    
 
     @Test
-    public void testUpdate() {
+    public void testUpdateValid() {
         System.out.println("update");
-        Long loaiDuLichId = 8L;
+        Long loaiDuLichId = 13L;
         String tenLoaiDuLich = "Du lịch ngoài nước";
         ILoaiDuLichBLL instance = new LoaiDuLichBLL();
         LoaiDuLichDTO loaiDuLich = instance.findById(loaiDuLichId);
@@ -68,17 +147,63 @@ public class ILoaiDuLichBLLTest {
         LoaiDuLichDTO result = instance.findById(loaiDuLichId);
         assertEquals(tenLoaiDuLich, result.getTenLoaiDuLich());
     }
-
+    
+    @Test
+    public void testUpdateInValid() {
+        System.out.println("update");
+        Long loaiDuLichId = 13L;
+        String tenLoaiDuLich = "!@#!#!123";
+        ILoaiDuLichBLL instance = new LoaiDuLichBLL();
+        LoaiDuLichDTO loaiDuLich = instance.findById(loaiDuLichId);
+        String oldTenLoaiDuLich = loaiDuLich.getTenLoaiDuLich();
+        loaiDuLich.setTenLoaiDuLich(tenLoaiDuLich);
+        instance.update(loaiDuLich);
+        LoaiDuLichDTO result = instance.findById(loaiDuLichId);
+        assertEquals(oldTenLoaiDuLich, result.getTenLoaiDuLich());
+    }
+    
+    @Test
+    public void testUpdateNull() {
+        System.out.println("update");
+        Long loaiDuLichId = 11L;
+        String tenLoaiDuLich = null;
+        ILoaiDuLichBLL instance = new LoaiDuLichBLL();
+        LoaiDuLichDTO loaiDuLich = instance.findById(loaiDuLichId);
+        String oldTenLoaiDuLich = loaiDuLich.getTenLoaiDuLich();
+        loaiDuLich.setTenLoaiDuLich(tenLoaiDuLich);
+        instance.update(loaiDuLich);
+        LoaiDuLichDTO result = instance.findById(loaiDuLichId);
+        assertEquals(oldTenLoaiDuLich, result.getTenLoaiDuLich());
+    }
+    
+    @Test
+    public void testUpdateNotExistId() {
+        System.out.println("update");
+        Long loaiDuLichId = 130L;
+        String tenLoaiDuLich = "Du lịch ngoài nước";
+        ILoaiDuLichBLL instance = new LoaiDuLichBLL();
+        LoaiDuLichDTO loaiDuLich = instance.findById(loaiDuLichId);
+        assertNull(loaiDuLich);
+    }
+    
     @Test
     public void testDelete() {
-        System.out.println("delete");  
-        Long id = 8L;
+        System.out.println("delete");
+        Long id = 12L;
         ILoaiDuLichBLL instance = new LoaiDuLichBLL();
         instance.delete(id);
         LoaiDuLichDTO result = instance.findById(id);
         assertNull(result);
     }
-
-  
+    
+    @Test
+    public void testDeleteNotExistId(){
+        System.out.println("delete");
+        Long id = 800L;
+        ILoaiDuLichBLL instance = new LoaiDuLichBLL();
+        instance.delete(id);
+        LoaiDuLichDTO result = instance.findById(id);
+        assertNull(result);
+    }
     
 }
